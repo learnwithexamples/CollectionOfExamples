fb6f19ded18aeaf9b8ba4a4bd4fff405c0b68463
Learn with Examples <learnwithexamples@outlook.com>
2022-10-20 21:20:01 -0700

    initial commit

---
+++ CollectionOfExamples/CollectionOfExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionOfExamplesApp.swift
+//  CollectionOfExamples
+//
+//  Created by Learn with Examples on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CollectionOfExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
